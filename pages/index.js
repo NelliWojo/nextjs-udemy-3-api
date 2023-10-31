1e5a3a70b11252e4d9649de038e79bffc9d6e7a3
NelliWojo <nelli.timorshina@wojo.com>
2023-10-31 16:44:01 +0100

    :bricks: api routes - post

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,40 @@
+import { useRef } from "react";
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, feedback: enteredFeedback };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef} />
+        </div>
+        <button onClick={submitHandler}>Submit</button>
+      </form>
+    </div>
+  );
 }
